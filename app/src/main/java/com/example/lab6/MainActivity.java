1ec9b1ad6857310cdbbe9b0e1020566493cc1b76
Ana Klabjan <39418477+aklabjan@users.noreply.github.com>
2021-10-30 16:29:27 -0500

    Milestone 1

---
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final LatLng mDestinationLatLng = new LatLng(43.0753151,-89.4056242);
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions()
+            .position(mDestinationLatLng)
+            .title("Destination"));
+
+        });
+
+    }
+}
